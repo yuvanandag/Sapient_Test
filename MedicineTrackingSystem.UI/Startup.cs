b414fbfddedd95f6c58e2bc5520befc5e9b3b8f9
yuvanandag <yuvanandagopal@gmail.com>
2020-12-21 10:53:58 +0530

    Updated latest changes

--- MedicineTrackingSystem.UI/Startup.cs
+++ MedicineTrackingSystem.UI/Startup.cs
@@ -42,12 +42,12 @@ namespace MedicineTrackingSystem.UI
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllerRoute(
+            //        name: "default",
+            //        pattern: "{controller=Home}/{action=Index}/{id?}");
+            //});
         }
     }
 }
